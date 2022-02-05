b9e71b55be8c53aaf25f5c40f158fff11e7f2b6a
FEDOROVA Maria <marie.fedorova@gmail.com>
2022-02-05 16:42:43 -0600

    fix cover img

--- src/App.svelte
+++ src/App.svelte
@@ -69,7 +69,6 @@
     colorVar = "const"
   }
 
-
   onMount(resize)
 
   onMount(async () => {
@@ -112,55 +111,6 @@
       },
     })
 
-    gsap.to("#main-title", {
-      // xPercent: -200,
-      yPercent: -700,
-      ease: "power2.out",
-      // duration: 10,
-      scrollTrigger: {
-        trigger: "#svgIntro",
-        start: "93% 99%",
-        end: "+=1000",
-        scrub: 1,
-      },
-    })
-
-    gsap.to("#subtitle", {
-      yPercent: -2000,
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: "#svgIntro",
-        start: "93% 99%",
-        end: "+=1000",
-        scrub: 1,
-      },
-    })
-    gsap.to("#logo-title", {
-      yPercent: -1500,
-      scrollTrigger: {
-        trigger: "#svgIntro",
-        start: "93% 99%",
-        end: "+=1000",
-        scrub: 1,
-      },
-    })
-    gsap.to(".cover-text", {
-      opacity: 0,
-      scrollTrigger: {
-        trigger: "#svgIntro",
-        start: "93% 99%",
-        end: "+=1000",
-      },
-    })
-    gsap.to(".cover-rect", {
-      opacity: 0,
-      scrollTrigger: {
-        trigger: "#svgIntro",
-        start: "93% 99%",
-        end: "+=1000",
-      },
-    })
-
     ScrollTrigger.create({
       trigger: "#canvas",
       endTrigger: "#step-3",
@@ -168,19 +118,16 @@
       end: "#step-3",
       pin: true,
       pinSpacing: false,
-      markers: true,
     })
 
     ScrollTrigger.create({
       trigger: "#step-1",
       start: "bottom 50%",
-      markers: true,
     })
 
     ScrollTrigger.create({
       trigger: "#step-2",
       start: "top 95%",
-      markers: true,
       onEnter: step1,
       onLeaveBack: step1Back,
     })
@@ -188,7 +135,6 @@
     ScrollTrigger.create({
       trigger: "#step-3",
       start: "top 95%",
-      markers: true,
       onEnter: step2,
       onLeaveBack: step2Back,
     })
@@ -196,190 +142,76 @@
 </script>
 
 <svelte:window on:resize={resize} />
-<div class="intro" bind:this={width}>
-  <svg id="svgIntro" width="100%">
-    <mask id="mask">
-      <path
-        id="wobble"
-        d="M4167.3,866.6l-45.7-2.6c-45.5-2.1-137.2-8.2-219.1,13.3c-82.2,20.7-154.6,68.9-231.7,103.9
-	c-76.8,35.8-158.4,58.3-239.4,82.3c-81,23.9-162.6,46.5-237.7,87.2c-75.3,40.1-144.6,97-226.7,118.1c-82.1,21.1-176.8,6.4-259.7,25
-	c-82.7,19.2-154.1,70.4-232.6,101.5c-78.5,31.1-165.1,39.3-241.9,75.1c-77.1,35-144.4,97.6-221.6,132.5
-	c-76.8,35.8-163.5,44-242.8,72.7c-79.7,27.8-152.1,76.1-234.3,96.7c-81.9,21.5-173.6,15.4-253.8,41.8
-	c-80.4,25.8-149.7,82.7-226.7,118.1c-77,35.4-161.6,49.3-241.9,75.1c-80.2,26.4-156.6,63.2-237.7,87.2s-167.7,32.1-246.2,63.3
-	c-78.5,31.1-149.9,82.3-224.2,125.2c-74.6,42.1-152.1,76.1-231.7,103.9c-79.4,28.7-160.9,51.2-239.4,82.3s-155,67.9-233.5,99
-	c-78.5,31.1-160,53.6-240.2,80c-80.4,25.8-159.9,54-239.4,82.3c-79.5,28.2-159,56.5-244.5,68c-85.3,12.1-177,5.9-222.5,3.9l-6.3-0.4
-	c-24.4-1.4-43.3-13.2-49.6-30.9l-1382.7,32l-1557.3-1782.4C-5077.8-460.3-971.7-7042.1,3974-1657.5l707.9,1955.2L4167.3,866.6z"
-        transform="translate(500,150)"
-        fill="white"
-      />
-    </mask>
-    <image
-      href="images/3I1A9619.jpeg"
-      width="100%"
-      height="100%"
-      preserveAspectRatio="xMidYMid slice"
-      mask="url(#mask)"
+<!-- <div class="intro" bind:this={width}> -->
+<!-- <div class="img-bg" /> -->
+<div class="intro-container">
+  <img
+    class="img-bg"
+    id="intro-img"
+    src="images/3I1A9619.jpg"
+    alt="house with a scenic view"
+  />
+  <div class="intro-paragraph">
+    <img
+      id="img-logo-nepal"
+      width="65px"
+      src="images/logoCropped.png"
+      alt="logo build up nepal"
     />
-
-    <svg id="svgTitle" x="23%" y="15%" overflow="visible">
-      {#if width > 800}
-        <text id="main-title" x="0%" y="65px">Build-up Nepal</text>
-        <text id="subtitle" x="40px" y="95px">an interactive story</text>
-        <image
-          id="logo-title"
-          href="images/logoCropped.png"
-          x="-80px"
-          y="1px"
-          width="75px"
-        />
-        <rect
-          class="cover-rect"
-          x="-5px"
-          y="120px"
-          width="620"
-          height="245"
-          fill="rgb(35, 63, 153)"
-          opacity="0.5"
-        />
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="150px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >Build up Nepal is on a mission to break the vicious poverty cycle in
-          rural Nepal through
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="170px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >safe houses and long-term jobs for poor families.This is important
-          because millions
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="190px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >of families in rural Nepal live in deep poverty and are forced to
-          migrate from their
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="210px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >villages due to lack of jobs, housing, and economic development. The
-          construction
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="230px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >industry in Nepal are growing rapidly, presenting a unique
-          opportunity to create jobs
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="250px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >for poor families. But the conventional fired brick industry is
-          responsible for poor
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="270px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >working conditions, child labor and 37% of CO2 emissions from
-          combustion in Nepal.
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="300px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >Build up Nepal’s solution - innovative micro-enterprise model
-          combining two solutions:
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="320px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >(1) Low-cost, eco-friendly technology, safe housing for poor families
-        </text>
-        <text
-          class="cover-text"
-          x="0.5%"
-          y="340px"
-          width="600"
-          height="200"
-          fill="rgb(238, 238, 238)"
-          >(2) Innovative micro-enterprise model, creating jobs for poor
-          families
-        </text>
-      {/if}
-      <g transform="translate(0,380)" class="cover-text">
-        <path
-          d="M0.7,12.5l15,18.4c0.1,0.2,0.4,0.2,0.6,0L29,12.5"
-          fill="none"
-          stroke="rgb(238, 238, 238)"
-          stroke-width="2"
-        />
-        <path
-          d="M0.7,2.5l15,18.4c0.1,0.2,0.4,0.2,0.6,0L29,2.5"
-          fill="none"
-          stroke="rgb(238, 238, 238)"
-          stroke-width="2"
-        />
-        <text class="cover-text" x="50" y="20" fill="rgb(238, 238, 238)"
-          >Scroll to learn about our impact
-        </text>
-      </g>
-      {#if width < 801}
-        <text id="main-title" x="-5%" y="65px">Build-up</text>
-        <text id="main-title" x="-5%" y="150px">Nepal</text>
-        <text id="subtitle" x="-5%" y="220px">an interactive story</text>
-      {/if}
-    </svg>
-  </svg>
-  <div id="canvas" bind:this={width}>
-    <MapCanvass
-      {geo}
-      {data}
-      width={widthChart}
-      {whichX}
-      {whichY}
-      {fX}
-      {groupingVar}
-      {fill}
-      {entrepreneur}
-      {colorVar}
-    />
+    <h1 id="main-title">Build-up Nepal</h1>
+    <p id="subtitle">an interactive story</p>
+    <p class="intro-text">
+      Build up Nepal is on a mission to break the vicious poverty cycle in rural
+      Nepal through safe houses and long-term jobs for poor families.This is
+      important because millions of families in rural Nepal live in deep poverty
+      and are forced to migrate from their villages due to lack of jobs,
+      housing, and economic development. The construction industry in Nepal are
+      growing rapidly, presenting a unique opportunity to create jobsfor poor
+      families. But the conventional fired brick industry is responsible for
+      poorworking conditions, child labor and 37% of CO2 emissions
+      fromcombustion in Nepal.Build up Nepal’s solution - innovative
+      micro-enterprise model combining two solutions: (1) Low-cost, eco-friendly
+      technology, safe housing for poor families(2) Innovative micro-enterprise
+      model, creating jobs for poor families
+    </p>
+    <p class="scroll">&#8595  Scroll down to discover more about our impact</p>
   </div>
 </div>
+<!-- <svg>
+  <g transform="translate(0,380)" class="cover-text">
+    <path
+      d="M0.7,12.5l15,18.4c0.1,0.2,0.4,0.2,0.6,0L29,12.5"
+      fill="none"
+      stroke="rgb(238, 238, 238)"
+      stroke-width="2"
+    />
+    <path
+      d="M0.7,2.5l15,18.4c0.1,0.2,0.4,0.2,0.6,0L29,2.5"
+      fill="none"
+      stroke="rgb(238, 238, 238)"
+      stroke-width="2"
+    />
+    <text class="cover-text" x="50" y="20" fill="rgb(238, 238, 238)"
+      >Scroll to learn about our impact
+    </text>
+  </g>
+  <g /></svg
+> -->
+<div id="canvas" bind:this={width}>
+  <MapCanvass
+    {geo}
+    {data}
+    width={widthChart}
+    {whichX}
+    {whichY}
+    {fX}
+    {groupingVar}
+    {fill}
+    {entrepreneur}
+    {colorVar}
+  />
+</div>
+<!-- </div> -->
 <article class="scrolls">
   <section class="step" id="step-1">
     <p>
@@ -532,7 +364,7 @@
 <hr class="solid" />
 <div class="article" id="footer">
   <div>
-    <img width="400px" src="images/logo.png" alt="logo" />
+    <img id="vsg-logo" width="400px" src="images/logo.png" alt="logo" />
     <p class="byline">
       Created by <a href="https://twitter.com/mashfedorova" target="_blank"
         >Maria Fedorova</a
@@ -543,6 +375,50 @@
 </div>
 
 <style>
+  #intro-img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: relative;
+  }
+
+  .intro-container {
+    height: 100%;
+    position: relative;
+    text-align: left;
+    color: white;
+  }
+
+  .intro-paragraph {
+    position: absolute;
+    top: 50px;
+    left: 20%;
+    width: 60%;
+  }
+
+  .scroll {
+    color: rgb(238, 238, 238);
+  }
+
+  .intro-text {
+    box-shadow: 1px 1px 3px rgb(35, 63, 153, 0.5);
+    /* width: 80%; */
+    max-width: 800px;
+    /* position: relative; */
+    background-color: rgb(35, 63, 153, 0.5);
+    padding: 20px;
+    line-height: 1.4;
+    font-size: 1.2rem;
+    opacity: 0.9;
+  }
+
+  #img-logo-nepal {
+    display: inline;
+    position: absolute;
+    top: 60px;
+  }
+
   #footer {
     display: flex;
     justify-content: center;
@@ -550,7 +426,7 @@
     margin-bottom: 50px;
   }
 
-  img {
+  #vsg-logo {
     margin-bottom: 50px;
   }
   .byline {
@@ -647,34 +523,22 @@
     margin-bottom: 100px;
   }
 
-  #svgIntro {
-    position: absolute;
-    overflow: visible;
-    display: inline-block;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 110vh;
-    margin-bottom: 500px;
-  }
-
   #main-title {
-    font-family: "Noto Serif", serif;
-    /* font-family: "Bree Serif", serif; */
-    font-family: "Roboto", sans-serif;
+    font-family: "Noto", sans-serif;
     font-weight: bold;
     font-size: 5rem;
-    fill: rgb(35, 63, 153);
+    color: rgb(35, 63, 153);
     letter-spacing: 2px;
+    margin-left: 70px;
   }
 
   #subtitle {
-    font-family: "Alegreya Sans", sans-serif;
+    /* font-family: "Alegreya Sans", sans-serif; */
     letter-spacing: 0.2rem;
     font-weight: 600;
     font-size: 1.2rem;
-    fill: rgb(238, 238, 238);
+    color: rgb(238, 238, 238);
+    margin-top: -50px;
   }
 
   #canvas {
@@ -682,11 +546,7 @@
     width: 90%;
     display: flex;
     justify-content: center;
-    z-index: -11;
-  }
-
-  #step-1 {
-    margin-top: 500px;
+    margin-top: 200px;
   }
 
   .step {
@@ -696,12 +556,10 @@
     max-width: 600px;
     margin-bottom: 85vh;
     position: relative;
-    /* background-color: #e0e1e2; */
     background-color: #dadcdd;
     padding: 20px;
     line-height: 1.4;
     font-size: 1.2rem;
-    position: relative;
     opacity: 0.9;
   }
 
@@ -709,7 +567,6 @@
     max-height: 1em;
     display: inline;
     position: relative;
-    /* vertical-align: top; */
     overflow: visible;
   }
 
@@ -729,11 +586,6 @@
         format("woff");
     font-weight: normal;
     font-style: normal;
-  }
-
-  .cover-text {
-    font-size: 1.1rem;
-    fill: rgb(238, 238, 238);
   }
 
   hr {
